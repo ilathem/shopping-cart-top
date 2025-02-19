79029573d6ccc281a4842fd81c0ddbc6db994369
ilathem <isaiahlathem@gmail.com>
2025-02-19 00:33:51 -0600

    fix test

--- src/tests/nav.test.jsx
+++ src/tests/nav.test.jsx
@@ -3,18 +3,17 @@ import { render, screen } from '@testing-library/react';
 import Nav from '../components/nav';
 
 describe("Nav component", () => {
+  render(<Nav />);
+
   it('renders correct heading', () => {
-    render(<Nav />);
     expect(screen.getByRole('heading').textContent).toMatch(/Navigation/);
   })
 
   it('renders home link', () => {
-    render(<Nav />);
-    expect(screen.getByRole('link', {name: /home/i})).toMatch(/home/i);
+    expect(screen.getByRole('link', {name: /home/i}).textContent).toMatch(/home/i);
   })
 
   it('renders shop link', () => {
-    render(<Nav />);
-    expect(screen.getByRole('link', {name: /shop/i})).toMatch(/shop/i);
+    expect(screen.getByRole('link', {name: /shop/i}).textContent).toMatch(/shop/i);
   })
 })
