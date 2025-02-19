6fdfe534e4abc0c1c9357f799786bc985881f006
ilathem <isaiahlathem@gmail.com>
2025-02-19 02:17:28 -0600

    implement current page

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -5,14 +5,27 @@ export default function Nav() {
     <div className='border-2 border-red w-64 m-5 rounded-2xl p-4 text-center'>
       <p className='text-3xl border-b-3 border-zinc-700'>Navigation</p>
       <ul>
-        <li className='active:scale-90 text-2xl border-2 hover:border-zinc-500 border-zinc-700 cursor-pointer transition-all rounded-md my-2'>
-          <NavLink to='/' end>
+        <NavLink
+          to='/'
+          className={({ isActive }) =>
+            isActive ? 'text-zinc-200 transition-all' : 'transition-all'
+          }
+          end
+        >
+          <li className='active:scale-90 text-2xl border-2 hover:border-zinc-500 border-zinc-700 cursor-pointer transition-all rounded-md my-2'>
             Home
-          </NavLink>
-        </li>
-        <li className='active:scale-90 text-2xl border-2 hover:border-zinc-500 border-zinc-700 cursor-pointer transition-all rounded-md my-2'>
-          <NavLink to='/shop'>Shop</NavLink>
-        </li>
+          </li>
+        </NavLink>
+        <NavLink
+          to='/shop'
+          className={({ isActive }) =>
+            isActive ? 'text-zinc-200 transition-all' : 'transition-all'
+          }
+        >
+          <li className='active:scale-90 text-2xl border-2 hover:border-zinc-500 border-zinc-700 cursor-pointer transition-all rounded-md my-2'>
+            Shop
+          </li>
+        </NavLink>
       </ul>
     </div>
   );
