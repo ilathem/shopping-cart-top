5f808b72ac106d192b2fa7a9db27c788fdd51fdb
ilathem <isaiahlathem@gmail.com>
2025-02-19 01:40:11 -0600

    adjust test to account for routing

--- src/tests/nav.test.jsx
+++ src/tests/nav.test.jsx
@@ -1,47 +1,40 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, beforeEach } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import { RouterProvider, createMemoryRouter } from 'react-router';
 import userEvent from '@testing-library/user-event';
-import Nav from '../components/nav';
 import routesConfig from '../routes/routesConfig';
+
+beforeEach(() => { // Arrange in context of route component 
+  const router = createMemoryRouter(routesConfig, {
+    initialEntries: ['/'],
+  });
+  render(<RouterProvider router={router} />);
+});
 
 describe('Nav component', () => {
   it('renders correct heading', () => {
-    render(<Nav />);
     expect(screen.getByRole('paragraph').textContent).toMatch(/Navigation/);
   });
 
   it('renders home link', () => {
-    render(<Nav />);
     expect(screen.getByRole('link', { name: /home/i }).textContent).toMatch(
       /home/i
     );
   });
 
   it('renders shop link', () => {
-    render(<Nav />);
     expect(screen.getByRole('link', { name: /shop/i }).textContent).toMatch(
       /shop/i
     );
   });
 
   it('clicking home navigates to home', async () => {
-    const router = createMemoryRouter(routesConfig, { // Arrange
-      initialEntries: ['/'],
-    });
-    render(<RouterProvider router={router} />);
-
     await userEvent.click(screen.getByRole('link', { name: /home/i })); // Act
 
     expect(screen.getByRole('heading').textContent).toMatch(/Home/); // Assert
   });
 
   it('clicking shop navigates to shop', async () => {
-    const router = createMemoryRouter(routesConfig, { // Arrange
-      initialEntries: ['/'],
-    });
-    render(<RouterProvider router={router} />);
-
     await userEvent.click(screen.getByRole('link', { name: /shop/i })); // Act
 
     expect(screen.getByRole('heading').textContent).toMatch(/Shop/); // Assert
