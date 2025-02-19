e3d79ed2526a184817a5a607f362e708ec2fbe08
ilathem <isaiahlathem@gmail.com>
2025-02-19 00:45:43 -0600

    implement cleanup for each test

--- src/tests/nav.test.jsx
+++ src/tests/nav.test.jsx
@@ -2,18 +2,23 @@ import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import Nav from '../components/nav';
 
-describe("Nav component", () => {
-  render(<Nav />);
-
+describe('Nav component', () => {
   it('renders correct heading', () => {
+    render(<Nav />);
     expect(screen.getByRole('heading').textContent).toMatch(/Navigation/);
-  })
+  });
 
   it('renders home link', () => {
-    expect(screen.getByRole('link', {name: /home/i}).textContent).toMatch(/home/i);
-  })
+    render(<Nav />);
+    expect(screen.getByRole('link', { name: /home/i }).textContent).toMatch(
+      /home/i
+    );
+  });
 
   it('renders shop link', () => {
-    expect(screen.getByRole('link', {name: /shop/i}).textContent).toMatch(/shop/i);
-  })
-})
+    render(<Nav />);
+    expect(screen.getByRole('link', { name: /shop/i }).textContent).toMatch(
+      /shop/i
+    );
+  });
+});
--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   test: {
     environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/tests/setup.js',
   },
 })
