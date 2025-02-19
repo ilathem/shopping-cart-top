bf069400f874843e9b9e8389c7ab5e0f83b0b5f9
ilathem <isaiahlathem@gmail.com>
2025-02-19 01:25:49 -0600

    add route tests

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,7 +1,7 @@
 export default function Nav() {
   return (
     <div className='border-2 border-red w-64 m-5 rounded-2xl p-4 text-center'>
-      <h1 className='text-3xl border-b-3 border-zinc-700'>Navigation</h1>
+      <p className='text-3xl border-b-3 border-zinc-700'>Navigation</p>
       <ul >
         <li className='active:scale-90 text-2xl border-2 hover:border-zinc-500 border-zinc-700 cursor-pointer transition-all rounded-md my-2'>
           <a href="#">Home</a>
--- src/tests/nav.test.jsx
+++ src/tests/nav.test.jsx
@@ -1,11 +1,14 @@
 import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
+import { RouterProvider, createMemoryRouter } from 'react-router';
+import userEvent from '@testing-library/user-event';
 import Nav from '../components/nav';
+import routesConfig from '../routes/routesConfig';
 
 describe('Nav component', () => {
   it('renders correct heading', () => {
     render(<Nav />);
-    expect(screen.getByRole('heading').textContent).toMatch(/Navigation/);
+    expect(screen.getByRole('paragraph').textContent).toMatch(/Navigation/);
   });
 
   it('renders home link', () => {
@@ -20,5 +23,27 @@ describe('Nav component', () => {
     expect(screen.getByRole('link', { name: /shop/i }).textContent).toMatch(
       /shop/i
     );
+  });
+
+  it('clicking home navigates to home', async () => {
+    const router = createMemoryRouter(routesConfig, { // Arrange
+      initialEntries: ['/'],
+    });
+    render(<RouterProvider router={router} />);
+
+    await userEvent.click(screen.getByRole('link', { name: /home/i })); // Act
+
+    expect(screen.getByRole('heading').textContent).toMatch(/Home/); // Assert
+  });
+
+  it('clicking shop navigates to shop', async () => {
+    const router = createMemoryRouter(routesConfig, { // Arrange
+      initialEntries: ['/'],
+    });
+    render(<RouterProvider router={router} />);
+
+    await userEvent.click(screen.getByRole('link', { name: /shop/i })); // Act
+
+    expect(screen.getByRole('heading').textContent).toMatch(/Shop/); // Assert
   });
 });
