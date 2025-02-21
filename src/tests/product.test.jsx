cf14acf51c6f2d8342c06d64af83544d0fbf7d4f
ilathem <isaiahlathem@gmail.com>
2025-02-20 20:16:23 -0600

    implement product ui tests

---
+++ src/tests/product.test.jsx
@@ -0,0 +1,28 @@
+import { describe, it, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
+// import { RouterProvider, createMemoryRouter } from 'react-router';
+import Product from '../components/product';
+
+describe('Product component UI', () => {
+  it('renders image with placeholder in case of absent url', () => {
+    render(<Product />);
+    expect(screen.getByRole('img', { name: 'product-image' }).src).toMatch(
+      'https://placehold.co/600x400'
+    );
+  });
+
+  it('renders product title and description', () => {
+    render(<Product />);
+    expect(screen.getByRole('heading', { name: 'product-title' }));
+    expect(screen.getByRole('paragraph', { name: 'product-description' }));
+  });
+
+  it('contains form for updating quantity, increment and decrement buttons, and submit', () => {
+    render(<Product />);
+    expect(screen.getByLabelText('Quantity'));
+    expect(screen.getByRole('spinbutton', { name: 'quantity-input' }));
+    expect(screen.getByRole('button', { name: 'quantity-increment' }).textContent).toMatch('+1');
+    expect(screen.getByRole('button', { name: 'quantity-decrement' }).textContent).toMatch('-1');
+    expect(screen.getByRole('button', { name: 'quantity-submit' }));
+  });
+});
