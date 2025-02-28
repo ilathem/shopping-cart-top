58bf83ba09c3f8a9472e5758301d96c4b62118ff
ilathem <isaiahlathem@gmail.com>
2025-02-27 18:44:53 -0600

    implement test for api

--- src/tests/product.test.jsx
+++ src/tests/product.test.jsx
@@ -1,9 +1,27 @@
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, beforeAll, afterAll, afterEach } from "vitest";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { RouterProvider, createMemoryRouter } from "react-router";
 import routesConfig from "../routes/routesConfig";
 import Product from "../components/product";
+import { setupServer } from "msw/node";
+import { http, HttpResponse } from "msw";
+import { productItems } from "./productTestData";
+
+const handlers = [
+  // simulating fakestore api
+  http.get(`https://fakestoreapi.com/products`, () => {
+    return HttpResponse.json(productItems);
+  }),
+];
+
+const server = setupServer(...handlers);
+
+beforeAll(() => server.listen({ onUnhandledRequest: "error" })); // Start the fake server
+
+afterAll(() => server.close()); // Close the server
+
+afterEach(() => server.resetHandlers()); // For test isolation
 
 describe("Product component UI", () => {
   it("renders image with placeholder in case of absent url", () => {
@@ -15,8 +33,12 @@ describe("Product component UI", () => {
 
   it("renders product title and description", () => {
     render(<Product />);
-    expect(screen.getByRole("heading", { name: "product-title" }));
-    expect(screen.getByRole("paragraph", { name: "product-description" }));
+    expect(
+      screen.getByRole("heading", { name: "product-title" }),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole("paragraph", { name: "product-description" }),
+    ).toBeInTheDocument();
   });
 
   it("contains form for updating quantity, increment and decrement buttons, and submit", () => {
@@ -33,7 +55,7 @@ describe("Product component UI", () => {
   });
 });
 
-describe("Product component functionality", () => {
+describe("Product component UI functionality", () => {
   it("increment button increments by one", async () => {
     const user = userEvent.setup();
     render(<Product />);
@@ -97,15 +119,18 @@ describe("Product component functionality", () => {
 
   it("clicking add to cart adds specified quantity to cart", async () => {
     const router = createMemoryRouter(routesConfig, {
-      initialEntries: ["/shop"],
+      initialEntries: ["/"],
     });
     render(<RouterProvider router={router} />);
     const user = userEvent.setup();
+    await user.click(screen.getByRole("link", { name: "Shop" }));
     await user.click(
-      screen.getByRole("spinbutton", { name: "quantity-input" }),
+      screen.getAllByRole("spinbutton", { name: "quantity-input" })[0],
     );
     await user.keyboard("1");
-    await user.click(screen.getByRole("button", { name: "quantity-submit" }));
+    await user.click(
+      screen.getAllByRole("button", { name: "quantity-submit" })[0],
+    );
     expect(
       screen.getByRole("emphasis", { name: "cart-quantity" }).textContent,
     ).toMatch("1");
---
+++ src/tests/shop.test.jsx
@@ -0,0 +1,38 @@
+import userEvent from "@testing-library/user-event";
+import { describe, it, expect } from "vitest";
+import { beforeAll, afterAll, afterEach } from "vitest";
+import { setupServer } from "msw/node";
+import { http, HttpResponse } from "msw";
+import { render, screen } from "@testing-library/react";
+import { RouterProvider, createMemoryRouter } from "react-router";
+import routesConfig from "../routes/routesConfig";
+import { productItems } from "./productTestData";
+
+const handlers = [
+  // simulating fakestore api
+  http.get(`https://fakestoreapi.com/products`, () => {
+    return HttpResponse.json(productItems);
+  }),
+];
+
+const server = setupServer(...handlers);
+
+beforeAll(() => server.listen({ onUnhandledRequest: "error" })); // Start the fake server
+
+afterAll(() => server.close()); // Close the server
+
+afterEach(() => server.resetHandlers()); // For test isolation
+
+describe("Shop component api", () => {
+  it("correctly calls the api", async () => {
+    const router = createMemoryRouter(routesConfig, {
+      initialEntries: ["/"],
+    });
+    render(<RouterProvider router={router} />);
+    const user = userEvent.setup();
+    await user.click(screen.getByRole("link", { name: "Shop" }));
+    expect(
+      screen.getAllByRole("heading", { name: "product-title" })[0].textContent,
+    ).toMatch("Product title");
+  });
+});
